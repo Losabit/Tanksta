e56ab0bd1223f25a6188fbca1466f7627d93e6bd
Quentin Clodion <losabit@Noe.localdomain>
2020-06-26 19:26:18 +0000

    start correction of balistic algorithms

--- Project/bullet.py
+++ Project/bullet.py
@@ -13,7 +13,9 @@ class Bullet(pygame.sprite.Sprite):
         self.angle = angle
         self.puissance = puissance
         self.before_rect_y = ((-9.81 * math.pow(self.rect[0], 2)) / (2 * math.pow(self.puissance * math.cos(math.radians(self.angle)), 2)) + self.rect[0] * math.tan(math.radians(self.angle)))
-        
+        self.altitude_max = math.pow(self.puissance * math.sin(math.radians(self.angle)), 2) / (2 * 9.81) + self.rect[1]
+        self.chute = False
+
     def display(self, screen):
         screen.blit(self.image, self.rect)
         self.fire()
@@ -21,7 +23,14 @@ class Bullet(pygame.sprite.Sprite):
     def fire(self):
         self.rect[0] += self.puissance
         rect_y = ((-9.81 * math.pow(self.rect[0], 2)) / (2 * math.pow(self.puissance * math.cos(math.radians(self.angle)), 2)) + self.rect[0] * math.tan(math.radians(self.angle)))
-        self.rect[1] += (rect_y - self.before_rect_y) / 1000
+        
+        if self.rect[1] > self.altitude_max:
+            self.chute = True
+        if self.chute:
+            self.rect[1] -= (rect_y - self.before_rect_y) / 1000
+        else:
+            self.rect[1] += (rect_y - self.before_rect_y) / 1000
+        
         self.before_rect_y = rect_y
         print(self.rect[1])
-       
+        print(self.altitude_max )
--- Project/player.py
+++ Project/player.py
@@ -5,7 +5,8 @@ from tank import Tank
 min_puissance = 1
 max_puissance = 10
 value_puissance = 0.05
-ecart_angle = 20
+ecart_angle = 15
+canon_angle_value = 2
 move_value = 3
 
 class Player():
@@ -15,18 +16,35 @@ class Player():
         self.increase_puissance = False
         self.move = False
         self.direction = 0
+        self.can_move_canon = False
+        self.canon_direction = 0
+
 
     def wait(self):
         if self.increase_puissance:
             self.puissance += value_puissance
         if self.move and self.direction != 0:
             self.tank.move(move_value if self.direction > 0 else -move_value)
+        if self.can_move_canon and self.canon_direction != 0:
+            if self.canon_direction > 0 and self.tank.canon_angle > 180 + ecart_angle:
+                return
+            if self.canon_direction < 0 and self.tank.canon_angle < -ecart_angle:
+                return
+            self.tank.moveCanon(canon_angle_value if self.canon_direction > 0 else -canon_angle_value)
             
 
     def controller(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                self.increase_puissance = True
+
+            if event.key == pygame.K_q:
+                self.can_move_canon = True
+                self.canon_direction = 1
+            elif event.key == pygame.K_d:
+                self.can_move_canon = True
+                self.canon_direction = -1
+
             if event.key == pygame.K_RIGHT and self.move == False:
                 self.move = True
                 self.direction = 1
@@ -42,9 +60,9 @@ class Player():
                 self.increase_puissance = False
 
             if event.key == pygame.K_q and self.tank.canon_angle < 180 + ecart_angle:
-                self.tank.moveCanon(10)
+                self.can_move_canon = False
             elif event.key == pygame.K_d and self.tank.canon_angle > -ecart_angle:
-                self.tank.moveCanon(-10)
+                self.can_move_canon = False
 
             if event.key == pygame.K_RIGHT and self.direction > 0:
                 self.move = False
