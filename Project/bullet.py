1b59566263574dadf5cee142cb77efec5779ab0d
PaulBenabou <paulbenabou@gmail.com>
2020-07-21 18:36:32 +0200

    Fix bullet trajectory

--- Project/bullet.py
+++ Project/bullet.py
@@ -6,16 +6,26 @@ directory = str(os.path.abspath(os.getcwd())) + '/' #'/Project/'
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, coordonates, angle, puissance):
-        self.image = pygame.image.load(directory + 'assets/objects/tank_bullet5.png')
+        puissance = puissance * 19
+        self.image = pygame.image.load(
+            directory + 'assets/objects/tank_bullet5.png')
         self.origin_image = self.image
         self.rect = coordonates
         self.image = pygame.transform.rotate(self.origin_image, angle)
         self.angle = angle
         self.damage = 20
         self.puissance = puissance
-        self.altitude_max = self.rect[1] - 650 * math.sin(math.radians(self.angle)) 
-        self.altitude_start = self.rect[1]
-        self.chute = False
+
+        self.vx = puissance*math.cos(math.radians(angle))
+        self.vy = puissance*math.sin(math.radians(angle))
+
+        self.ax =  0#puissance # vent
+        self.ay = -9.8
+
+        self.x = self.rect[0]
+        self.y = self.rect[1]
+        self.time = 0
+      
         '''
         print("angle : " + str(self.angle))
         print("puissance : " + str(self.puissance))
@@ -23,20 +33,29 @@ class Bullet(pygame.sprite.Sprite):
         print("altitude max : " + str(self.altitude_max))
         '''
 
-    def display(self, screen):
+    def updateVx(self, dt):
+        self.vx = self.vx + self.ax*dt
+        return self.vx
+
+    def updateVy(self, dt):
+        self.vy = self.vy + self.ay*dt
+        return self.vy
+
+    def updateX(self,dt):
+        self.x = self.x + 0.5 * (self.vx + self.updateVx(dt)) * dt
+        return self.x
+
+    def updateY(self,dt):
+        self.y = self.y - 0.5 * (self.vy + self.updateVy(dt)) * dt
+        return self.y
+
+    def updateBulletPosition(self, dt):
+        self.rect[0] = self.updateX(dt)
+        self.rect[1] = self.updateY(dt)
+        print("X:",self.rect[0])
+        print("Y:",self.rect[1])
+        self.time = self.time + dt
+
+    def display(self, screen, delta):
         screen.blit(self.image, self.rect)
-        self.fire()
-
-    def fire(self):
-        self.rect[0] += self.puissance * math.cos(math.radians(self.angle)) 
-        
-        if self.rect[1] < self.altitude_max:
-            self.chute = True
-            self.rect[1] = self.altitude_max
-
-        #coeff = 1 - math.sin((self.rect[1] * math.pi / 2) / self.altitude_max)
-        if self.chute:
-            self.rect[1] += self.puissance * math.sin(math.radians(abs(self.angle))) 
-        else:
-            self.rect[1] -= self.puissance * math.sin(math.radians(self.angle))
-        
+        self.updateBulletPosition(delta)
